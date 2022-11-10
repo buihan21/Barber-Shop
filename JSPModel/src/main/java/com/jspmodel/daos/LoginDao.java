b8527fb3d8bfb0a226780c73e63a32c0d674846d
buihan21 <115077916+buihan21@users.noreply.github.com>
2022-11-10 22:35:23 +0700

    Add files via upload

--- JSPModel/src/main/java/com/jspmodel/daos/LoginDao.java
+++ JSPModel/src/main/java/com/jspmodel/daos/LoginDao.java
@@ -1,33 +1,55 @@
 package com.jspmodel.daos;
 
+import com.jspmodel.db.DBConnect;
+import com.jspmodel.models.User;
+
 import java.sql.*;
 
+//public class LoginDao {
+//    public static boolean validate(String name, String pass){
+//        boolean status = false;
+//        try {
+//            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//            System.out.println("Driver Registered");
+//            Connection con = DriverManager.getConnection("jdbc:sqlserver://MSI\\MSSQLSERVER;databaseName=barberShop;encrypt=true;trustServerCertificate=true;","sa", "sa");
+//            System.out.println("Connection successfully");
+//
+//            String sql = "SELECT role_id, user_name, pass_word" + " FROM [User] WHERE user_name = ? AND pass_word = ?";
+//            PreparedStatement stm = con.prepareStatement(sql);
+//            stm.setString(1, name);
+//            stm.setString(2, pass);
+//
+//            ResultSet rs = stm.executeQuery();
+//            status = rs.next();
+//        } catch (SQLException | ClassNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//        return status;
+//    }
+
+//    public static void main(String[] args) {
+//        System.out.print(validate("NgocJr", "12092002"));
+//    }
+//}
+
 public class LoginDao {
-    public static boolean validate(String name, String pass, int q){
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    public boolean checkLogin(String name, String pass){
         boolean status = false;
         try {
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            System.out.printf("Driver Registered");
-            Connection con = DriverManager.getConnection("jdbc:sqlserver://MSI\\MSSQLSERVER;databaseName=barberShop;encrypt=true;trustServerCertificate=true;","sa", "sa");
-            System.out.printf("Connection successfully");
-
-            PreparedStatement ps = con.prepareStatement("");
-            System.out.printf(name + " " + pass);
+            String query =  "SELECT role_id, user_name, pass_word" + " FROM [User] WHERE user_name = ? AND pass_word = ?";
+            conn = new DBConnect().getConnection();
+            ps = conn.prepareStatement(query);
             ps.setString(1, name);
-            ps.setString(2,pass);
-            ps.setInt(3, q);
-
-
-            //ResultSet rs = ps.executeQuery();
-            //status = rs.next();
-            //System.out.printf(String.valueOf(status));
-        } catch (SQLException | ClassNotFoundException e) {
+            ps.setString(2, pass);
+            rs = ps.executeQuery();
+            status = rs.next();
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return status;
-    }
-
-    public static void main(String[] args) {
-        System.out.printf(String.valueOf(validate("Hieu", "66866686", 1)));
     }
 }
--- JSPModel/src/main/java/com/jspmodel/daos/UserDao.java
+++ JSPModel/src/main/java/com/jspmodel/daos/UserDao.java
@@ -18,7 +18,7 @@ public class UserDao {
         return con;
     }
 
-    public static int insertUser(User u) throws ClassNotFoundException, SQLException {
+    public static void insertUser(User u) throws ClassNotFoundException, SQLException {
         Connection con = UserDao.getConnection();
         System.out.println("Conection Established Successfully");
         PreparedStatement ps = con.prepareStatement("insert into User(id, role_id, user_name ,pass_word, name, phone_number) values(?,?,?,?,?,?)");
@@ -30,7 +30,6 @@ public class UserDao {
         ps.setString(6, u.getPhone_number());
         int y = ps.executeUpdate();
         con.close();
-        return y;
     }
 
     public static User UserSearch(int u_id) throws ClassNotFoundException, SQLException {
@@ -41,15 +40,11 @@ public class UserDao {
         PreparedStatement ps = con.prepareStatement("SELECT * FROM User WHERE id=?");
         ps.setInt(1, u_id);
         ResultSet rs = ps.executeQuery();
+// co the them thong tin lay ra sau
         while (rs.next()) {
-            User e2 = new User();
-            e2.setId(rs.getInt("id"));
-            e2.setRole_id((rs.getInt("role_id")));
-            e2.setUser_name(rs.getString("user_name"));
-            e2.setPass_word(rs.getString("password"));
-            e2.setName(rs.getString("name"));
-            e2.setPhone_number(rs.getString("phone_number"));
+            u.setUser_name(rs.getString("user_name"));
         }
+
         System.out.println("name " + u.getUser_name());
         con.close();
         return u;
@@ -59,6 +54,8 @@ public class UserDao {
 
         Connection con = UserDao.getConnection();
         System.out.println("Conection Established Successfully updateUserInfo");
+        //id, name, age, mobNo, address, gender, bloodgroup, symptoms, department, doa, username, password
+
         PreparedStatement ps = con.prepareStatement("update User set id= ?, role_id = ?, user_name = ?,pass_word = ?, name = ?, phone_number = ? where id=?");
         ps.setInt(1, u.getId());
         ps.setInt(2, u.getRole_id());
@@ -72,7 +69,7 @@ public class UserDao {
         return u5;
     }
 
-    public static int deleteUser(int u_id) throws ClassNotFoundException, SQLException {
+    public static int deleteuser(int u_id) throws ClassNotFoundException, SQLException {
         Connection con = UserDao.getConnection();
         System.out.println("Conection Established Successfully");
         PreparedStatement ps = con.prepareStatement("delete from User where id=?");
@@ -103,17 +100,15 @@ public class UserDao {
 // test
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         User u = new User(2, 2, "user_name", "pass_word", "hoang", "124356");
+        insertUser(u);
+        User u1 = UserSearch(2);
         User u2 = new User(3, 3, "user_name", "pass_word", "a", "1243567");
-        insertUser(u);    
         insertUser(u2);
-        User u1 = UserSearch(1);
-        User u2_fixed = new User(3, 3, "user_name", "pass_word", "b", "1243567");
-        updateUserInfo(u2_fixed);
-        ArrayList<User> a = getUserRecords(1, 2);
+        ArrayList<User> a = new ArrayList<>();
         for(User u3 : a)
         {
             System.out.println(u3.getName());
         }
-        deleteUser(3);
+        deleteuser(3);
     }
 }
